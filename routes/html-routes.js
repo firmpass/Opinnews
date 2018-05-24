dff003703be33f444d0818715533e613d1debfe3
Eric "ET" Thornton <firmonomics@gmail.com>
2018-05-23 22:53:01 -0400

    created package.json, establishedroutes folder and html-routes.js, server.js logic index and main setup, index.handlebars route works properly. error handling needed.

---
+++ routes/html-routes.js
@@ -0,0 +1,19 @@
+// *********************************************************************************
+// html-routes.js - this file offers a set of routes for sending users to the various html pages
+// *********************************************************************************
+
+// Dependencies
+// =============================================================
+var path = require("path");
+
+// Routes
+// =============================================================
+module.exports = (app) => {
+
+    // Each of the below routes just handles the HTML page that the user gets sent to.
+
+    //index route loads home.html
+    app.get("/", (req, res) => {
+        res.sendFile(path.join(__dirname, "../views/index.handlebars"));
+    });
+};
---
+++ server.js
@@ -0,0 +1,46 @@
+//Server.js - This file is the initial starting point for the Node/Express server.
+// *************************
+// *** Dependencies
+// *************************
+
+//Sets up the Express App
+const express = require("express");
+const bodyParser = require("body-parser");
+const mysql = require("mysql");
+const exphbs = require("express-handlebars");
+const sequelize = require("sequelize");
+
+const app = express();
+const PORT = process.env.PORT || 8081;
+
+// Requiring our models for syncing 
+// ***HERE***
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.json());
+
+
+// Static directory
+app.use(express.static("public"));
+
+// Routes 
+// ======================================================
+// require("./routes/html-routes.js")(app);
+// require("./routes/*****")(app);
+// require("./routes/*****")(app);
+// require("./routes/*****")(app);
+
+// ======================================================
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+ 
+app.get('/', function (req, res) {
+    res.render('index');
+});
+ 
+app.listen(PORT, () => {
+    console.log("App listening on PORT " + PORT);
+});
+
+
