2626f2de28fc618cc9c198d09a1e74e8fa328db3
Eric "ET" Thornton <firmonomics@gmail.com>
2018-06-03 21:09:17 -0400

    Commits from dev2

--- models/post.js
+++ models/post.js
@@ -1,44 +1,3 @@
-<<<<<<< HEAD
-module.exports = function (sequelize, DataTypes) {
-    
-
-
-var Userpost = sequelize.define('Userpost', { 
-    userName: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-            notEmpty: true,
-        }
-    },
-    newPost: {
-        type: DataTypes.TEXT,
-        allowNull: false,
-    },
-    legit: {
-        type: DataTypes.INTEGER,
-    },
-    seemslegit: {
-        type: DataTypes.INTEGER,
-    },
-    fakeNews: {
-        type: DataTypes.INTEGER,
-    },
-    user_id: {
-        type: DataTypes.STRING
-    }
-//   }, {
-//       getterMethods: {
-//           fullName: () => {
-//             return this.firstName + ' ' + this.lastName;
-        
-//       }
-//   }
-   
-})
-    return Userpost;
-}
-=======
 module.exports = function(sequelize, DataTypes) {
     var Post = sequelize.define("Post", {
         user: {
@@ -69,4 +28,3 @@ module.exports = function(sequelize, DataTypes) {
 
     return Post;
 };
->>>>>>> 09bc7515259420524a44025fbb399859bf60f044
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,45 +1,7 @@
-<<<<<<< HEAD
-// *********************************************************************************
-// html-routes.js - this file offers a set of routes for sending users to the various html pages
-// *********************************************************************************
-
-// Dependencies
-// =============================================================
-var path = require("path");
-
-// Routes
-// =============================================================
-module.exports = (app) => {
-
-    // Each of the below routes just handles the HTML page that the user gets sent to.
-
-    //index route loads home.html
-    app.get("/", (req, res) => {
-        console.log("index route successful");
-        res.render("index");
-    });
-
-    app.get("/login", (req, res) => {
-        console.log("login route successful");
-        res.sendFile(path.join(__dirname, "../views/login.handlebars"));
-    });
-
-    app.get("/user-post", (req, res) => {
-        console.log("user-post route successful");
-        res.sendFile(path.join(__dirname, "../views/user-post.handlebars"));
-    });
-
-    app.post('/user-post/submit', (req, res, next) => {
-        res.redirect('/user-post');
-        console.log('User post successful');
-    });
-
-=======
 var post = require("../models/post");
 
 module.exports = function (app) {
     app.get("/", function(req, res) {
             res.render("index", req.body);
     });
->>>>>>> 09bc7515259420524a44025fbb399859bf60f044
 };
